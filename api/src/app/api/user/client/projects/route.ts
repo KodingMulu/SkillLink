8a55af1faa85f7a7c21b60e9b94edfdba2919fe4
NzrlAfndi <fbotzyt@gmail.com>
2025-12-14 20:56:56 +0700

    feature: projects client

--- api/src/app/api/user/client/projects/route.ts
+++ api/src/app/api/user/client/projects/route.ts
@@ -1,22 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { getAuthUser } from "@/lib/server-auth";
 
-export async function POST(req:Request) {
-     const data = await req.json();
+export async function GET(req: NextRequest) {
+  try {
+    const user = getAuthUser(req);
+    if (!user || user.role !== "CLIENT") {
+      return NextResponse.json({ message: "Forbidden", code: 403 }, { status: 403 });
+    }
 
-     const project = await prisma.project.create({
-          data: {
-               clientId: data.clientId,
-               title: data.title,
-               description: data.description,
-      budget: data.budget
-      duration: data.duration,
-      skills: data.skills,
-      deadline: data.deadline,
-      attachmentUrl: data.attachmentUrl,
-      status: "OPEN",
-          }
-     })
+    const activeProjects = await prisma.project.findMany({
+      where: {
+        job: {
+          clientId: user.id,
+        },
+      },
+      include: {
+        job: {
+            select: {
+                title: true,
+                budget: true
+            }
+        },
+        freelancer: {
+            select: {
+                id: true,
+                username: true,
+                email: true
+            }
+        }
+      },
+      orderBy: {
+        status: "asc",
+      },
+    });
 
-     return NextResponse.json(project)
+    return NextResponse.json({
+      message: "Success fetching active projects",
+      code: 200,
+      data: activeProjects,
+    });
+
+  } catch (error) {
+    return NextResponse.json({ message: "Internal Server Error", code: 500 }, { status: 500 });
+  }
 }
